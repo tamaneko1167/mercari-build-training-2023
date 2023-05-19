30fe0955d95ac9ae103ddf72db597cee19e5a2d7
tamaneko1167 <yuritamaneko1167@gmail.com>
2023-05-19 19:17:43 +0900

    complete step3

--- python/main.py
+++ python/main.py
@@ -1,7 +1,11 @@
 import os
 import logging
 import pathlib
-from fastapi import FastAPI, Form, HTTPException
+import json
+import hashlib
+import shutil
+from typing import Union
+from fastapi import FastAPI, Query, Form, File, UploadFile, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -22,9 +26,50 @@ app.add_middleware(
 def root():
     return {"message": "Hello, world!"}
 
+@app.get("/items/")
+def getItem():
+    with open('items.json') as f:
+        message = json.load(f)
+    return {f"{message}"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id):
+    with open('items.json') as f:
+        json_data = json.load(f)
+        item = json_data["items"][int(item_id)-1]
+        print(item)
+    return {f"{item}"}
+    
+
+# @app.get("/items/<items_id>")
+# def getItem():
+#     with open('items.json') as f:
+#         message = json.load(f)
+#     return {f"{message}"}
+
 @app.post("/items")
-def add_item(name: str = Form(...)):
-    logger.info(f"Receive item: {name}")
+def add_item(name: str = Form(...), category:str = Form(...), image:UploadFile = File(...)):
+    logger.info(f"Receive item: {name}, category: {category}")
+
+    #画像の処理
+    hash256_image = hashlib.sha256(repr(image).encode()).hexdigest()
+    image_name = hash256_image + ".png"
+    if image:
+        fileobj = image.file
+        upload_dir = open(os.path.join('images', image_name),'wb+')
+        shutil.copyfileobj(fileobj, upload_dir)
+        upload_dir.close()
+        return {"アップロードファイル名": image_name}
+
+    new_data = {"name": name, "category": category, "image_filename": image_name}
+
+    if os.path.exists('items.json'):
+        with open('items.json', "r", encoding="utf-8") as f:
+            json_data = json.load(f)
+            f.close()
+    with open('items.json', "w", encoding="utf-8") as f:
+        json_data["items"].append(new_data)
+        json.dump(json_data, f, ensure_ascii=False , sort_keys=False)
     return {"message": f"item received: {name}"}
 
 @app.get("/image/{image_filename}")
